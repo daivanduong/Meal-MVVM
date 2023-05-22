afcc7dd9a108efc41bc250eee1ec1578a29a94b6
daivanduong <dai22992@gmail.com>
2023-05-22 09:47:59 +0700

    Initial Commit

---
+++ Meal-MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Meal-MVVM
+//
+//  Created by Ocean97 on 22/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
